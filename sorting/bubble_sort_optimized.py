7a658706e55a29cbc0a554678a0a0bbe75523a3c
Vivek Chowgule <chowgulevivek02@gmail.com>
2025-12-17 23:44:23 +0530

    Docstrings added to all

--- sorting/bubble_sort_optimized.py
+++ sorting/bubble_sort_optimized.py
@@ -2,7 +2,7 @@ def bubble_sort(arr):
     """
     Sort given array of size n
     Best Case: 
-        No. of comparisions: n * (n-1) / 2
+        No. of comparisions: (n-1)
         No. of swaps: 0
     Worst Case:
         No. of comaparisions: n * (n-1) / 2
@@ -14,12 +14,16 @@ def bubble_sort(arr):
     Returns:
         arr: sorted array
     """
-    for i in range(len(arr)-1):
-        for j in range(len(arr)-1-i):
+    for i in range(len(arr) - 1):
+        flag = 0
+        for j in range(len(arr) - 1 - i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                flag = 1
+        if flag == 0:
+            return arr
     return arr
 
 if __name__ == "__main__":
-    l = [3, 2, 6, 7, 1, 9, 34, 76, 12, 90, 65]
+    l = [3, 5, 1 , 8, 2, 6, 90, 356, 23, 54, 67]
     print(bubble_sort(l))
