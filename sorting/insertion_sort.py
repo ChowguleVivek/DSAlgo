e9a9b99fcb5da3f47c5220541290c1e32927e20d
Vivek Chowgule <chowgulevivek02@gmail.com>
2025-12-19 14:27:11 +0530

    insertion sort added with docstring

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,10 +1,23 @@
 def insertion_sort(arr):
+    """
+    Sort given array of size n
+    Best Case: 
+        No. of comparisions: (n-1)
+        No. of swaps: 0
+    Worst Case:
+        No. of comaparisions: n * (n-1) / 2
+        No. of swaps: n * (n-1) / 2
+
+    parameters:
+        arr: input array
+
+    Returns:
+        arr: sorted array
+    """
     for i in range(1, len(arr)):
         key = arr[i]
         j = i
         while j > 0 and arr[j-1] > key:
-            # print(l)
-            # print("j ", j, "i ", i)
             arr[j] = arr[j-1]
             j -= 1
         arr[j] = key
